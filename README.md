69f5aa40680e4be71530ef0bb2f88fa81fb23817
IsabelaOliveira272018 <isabela.barros@compasso.com.br>
2020-06-21 16:24:55 -0300

    introduction

---
+++ README.md
@@ -0,0 +1,4 @@
+# javascript_introductionClass
+
+Introduction classes of javascript.
+The course is offered to the initiative of Digital Innovation courses for programming languages.
---
+++ javascript/main.js
@@ -0,0 +1,10 @@
+
+var nome = "Isa";
+var idade = 14
+var idade2 = 14;
+var frase = "Japão é o melhor time do mundo";
+alert("Bem vindo! " + nome + " você tem " + idade + " anos" + " mas sabemos que o javascript soma inteiros então idade com idade é: ");
+alert(idade + idade2)
+
+console.log(frase.replace("Japão", "Brasil"));
+
